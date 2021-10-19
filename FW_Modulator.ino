bff8b60b3728c8c1d348217aa4a2fd7df7244580
Oriol Pascual <69343366+OriolPascualOliver@users.noreply.github.com>
2021-10-19 19:14:34 +0200

    Update FW_Modulator.ino

--- FW_Modulator.ino
+++ FW_Modulator.ino
@@ -10,16 +10,26 @@
 //https://github.com/terryjmyers/PWM
 
 // GPIO's
-#define Update      4
-#define Data        A3
-#define SpeedSens   5
+#define SpeedSens   2
 #define Err         6
 #define LED         13
 #define PWM         3
 
-uint32_t freq = 25000;
+uint32_t freq = 16000;
 uint32_t MinPeriod = 10; max speed period in ms
 int Duty;
+
+int GetPulse(){
+    while(!digitalRead(SpeedSens)){
+   u
+    }
+    Ti=millis();
+    while(digitalRead(SpeedSens)){
+   
+    }
+    Tf=millis();
+    return int(Tf-Ti);
+}
 
 void setup(){
     pinMode(Err, OUTPUT);
@@ -27,7 +37,8 @@ void setup(){
     pinMode(SpeedSens, INPUT);
     pinMode(Data, INPUT);
     pinMode(Update, INPUT);
-
+    Serial.begin(9600);
+    //attachInterrupt(digitalPinToInterrupt(PIN), Funcio, RISING); //volatile a les variables
 
     InitTimersSafe(); 
     bool success = SetPinFrequencySafe(PWM, freq);
@@ -42,28 +53,12 @@ void setup(){
         digitalWrite(LED, LOW);
 
     }
-    while(!digitalRead(Update)){
-        delayMicroseconds(1000);
-    }
-    digitalWrite(LED, HIGH);
-    delay(150);
-    digitalWrite(LED, LOW);
-    delay(150);
-    digitalWrite(LED, HIGH);
-    delay(150);
-    digitalWrite(LED, LOW);
-
-
+    
 }
 void loop(){
-    if(!digitalRead(Update)){
-        Duty=analogRead(Data);
+    if(Serial.available()){
+        Duty=int(Serial.readString());
         pwmWrite(PWM, Dty);
-
-
-
-
-
     }
 
 
